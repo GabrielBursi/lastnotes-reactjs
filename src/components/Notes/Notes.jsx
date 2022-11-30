a86ca780bf4cfb44c47ad20ffec8f91bb4e9654c
Gabriel Bursi <gabriellbursii@gmail.com>
2022-11-30 17:29:58 -0300

    botao de editar nota

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,5 +1,6 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { DestaqueContext } from "../../context/DestaqueContext";
+import { NoteFormContext } from "../../context/NoteFormContext";
 import { NoteListContext } from "../../context/NoteListContext";
 
 import Note from "../Note/Note";
@@ -8,23 +9,36 @@ import './notes.css'
 
 function Notes() {
 
-    const { destaque, setDestaque } = useContext(DestaqueContext);
-    const { noteList } = useContext(NoteListContext)
+  const { destaque, setDestaque } = useContext(DestaqueContext);
+  const { noteList } = useContext(NoteListContext)
+  const { setTitle, setDesc } = useContext(NoteFormContext);
 
-    return (
-      <section>
-        {noteList.map(note => (
-          <Note
-            key={note.id}
-            id={note.id}
-            title={note.title}
-            description={note.description}
-            destaque={destaque}
-            setDestaque={setDestaque}
-          />
-        ))}
-      </section>
-    );
+  useEffect(() => {
+    if(destaque){
+      const noteComDestaque = noteList.find((note) => note.id === destaque);
+
+      setTitle(noteComDestaque.title);
+      setDesc(noteComDestaque.description);
+    }else{
+      setTitle('')
+      setDesc('')
+    }
+  }, [destaque]);
+
+  return (
+    <section>
+      {noteList.map(note => (
+        <Note
+          key={note.id}
+          id={note.id}
+          title={note.title}
+          description={note.description}
+          destaque={destaque}
+          setDestaque={setDestaque}
+        />
+      ))}
+    </section>
+  );
 }
 
 export default Notes;
